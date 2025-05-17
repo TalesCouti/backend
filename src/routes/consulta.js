0852900ef3446af14df0870dccec7c763e47dc5b
TalesCouti <talescouti40@gmail.com>
2025-05-17 16:34:55 -0300

    consulta

--- src/routes/consulta.js
+++ src/routes/consulta.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const { getConsulta, inserirConsulta } = require('../controllers/consultaController');
 const auth = require('../middleware/auth');
 
+<<<<<<< HEAD
 router.get('/consulta', auth, getConsulta);
 router.post('/consulta', auth, inserirConsulta);
+=======
+router.get('/', auth, getConsulta);
+router.post('/', auth, inserirConsulta);
+>>>>>>> parent of 1b6f11c (caminho errado de novo)
 
 module.exports = router;
