1b6f11cf6779ab3013cbbc599828c42321066146
TalesCouti <talescouti40@gmail.com>
2025-05-17 15:14:19 -0300

    caminho errado de novo

--- src/routes/consulta.js
+++ src/routes/consulta.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getConsulta, inserirConsulta } = require('../controllers/consultaController');
 const auth = require('../middleware/auth');
 
-router.get('/', auth, getConsulta);
-router.post('/', auth, inserirConsulta);
+router.get('/consultas', auth, getConsulta);
+router.post('/consultas', auth, inserirConsulta);
 
 module.exports = router;
