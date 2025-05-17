0429aa518689b4df9702e1afd628ae8ac5ade86f
TalesCouti <talescouti40@gmail.com>
2025-05-17 16:33:16 -0300

    .

--- src/routes/consulta.js
+++ src/routes/consulta.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getConsulta, inserirConsulta } = require('../controllers/consultaController');
 const auth = require('../middleware/auth');
 
-router.get('/consultas', auth, getConsulta);
-router.post('/consultas', auth, inserirConsulta);
+router.get('/consulta', auth, getConsulta);
+router.post('/consulta', auth, inserirConsulta);
 
 module.exports = router;
