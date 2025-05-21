e033079a857e3c3e96db812a9c2c2e69f7316407
TalesCouti <talescouti40@gmail.com>
2025-05-21 11:13:34 -0300

    auth

--- src/routes/medico.js
+++ src/routes/medico.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const medicoController = require('../controllers/medicoController');
+const auth = require('../middleware/auth');
 
 router.post('/login', medicoController.cadastrarMedico);
 router.post('/cadastro', medicoController.cadastrarMedico);
