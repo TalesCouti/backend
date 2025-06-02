5757521c24432017c8230d3ec7cc40ca8341c96b
TalesCouti <talescouti40@gmail.com>
2025-06-02 15:16:52 -0300

    router remover

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -7,6 +7,7 @@ router.post('/login', usuarioController.login);
 router.post('/cadastro', usuarioController.cadastro);
 router.post('/loginNfc', usuarioController.loginNfc);
 router.post('/cadastroNfc', auth, usuarioController.cadastroNFC);
+router.delete('/removerNFC', auth, usuarioController.removerNFC);
 router.get('/', auth, usuarioController.getUsuario);
 
 module.exports = router;
